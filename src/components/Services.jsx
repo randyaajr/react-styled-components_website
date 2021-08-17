d11ac5f8bfb2fca8e640eb59a7d75e291adc03cc
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-08-17 15:53:59 -0400

    Created a modal for the service video when viewing for small viewport

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -87,8 +87,29 @@ const Icon = styled.img`
   margin-right: 10px;
 `;
 
+const Modal = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  background-color: darkblue;
+  color: white;
+  padding: 15px;
+  border: none;
+  border-radius: 5px;
+  right: 5px;
+  top: 25%;
+`;
+
 const Services = () => {
   const [open, setOpen] = useState(false);
+  const smallScreen = window.screen.width <= 480 ? true : false;
   return (
     <Container>
       <Left>
@@ -121,6 +142,18 @@ const Services = () => {
           </Button>
         </Wrapper>
       </Right>
+      {smallScreen && open && (
+        <Modal>
+          <Video
+            open={open}
+            autoPlay
+            loop
+            controls
+            src="https://player.vimeo.com/external/449759244.sd.mp4?s=d5f3da46ddc17aa69a7de84f1e420610ebd2a391&profile_id=139&oauth2_token_id=57447761"
+          />
+          <CloseButton onClick={() => setOpen(false)}>Close</CloseButton>
+        </Modal>
+      )}
     </Container>
   );
 };
